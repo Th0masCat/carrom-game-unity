843c9b175b8e762f2ac97feb35d1fa663d359265
Th0masCat <catsaredogstoo@gmail.com>
2023-05-15 03:02:25 +0530

    temp ai system

--- Assets/Scripts/StrikerController.cs
+++ Assets/Scripts/StrikerController.cs
@@ -10,10 +10,13 @@ public class StrikerController : MonoBehaviour
     Slider StrikerSlider;
 
     [SerializeField]
-    float strikerSpeed = 1000f;
+    float strikerSpeed = 10000f;
 
     [SerializeField]
     Transform strikerForceField;
+
+    [SerializeField]
+    GameObject pocket;
 
     bool isCharging;
     Vector2 direction;
@@ -72,17 +75,62 @@ public class StrikerController : MonoBehaviour
 
     private void Update()
     {
-
         if (rb.velocity.magnitude > 0.1f)
         {
             StrikerSlider.gameObject.SetActive(false);
         }
-        else if (!isCharging)
+
+
+        if (!isCharging)
         {
             StartCoroutine(StrikerXPos());
-            StrikerSlider.gameObject.SetActive(true);
         }
 
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        // Determine which coin to hit based on game logic.
+        // For example, the AI could target the closest coin to the pocket, or a high-value coin.
+
+        GameObject[] coins = GameObject.FindGameObjectsWithTag("Black");
+        GameObject closestCoin = null;
+        float closestDistance = Mathf.Infinity;
+        foreach (GameObject coin in coins)
+        {
+            float distance = Vector3.Distance(coin.transform.position, pocket.transform.position);
+            if (distance < closestDistance)
+            {
+                closestCoin = coin;
+                closestDistance = distance;
+            }
+        }
+
+        // Calculate the direction and speed of the striker based on the position of the target coin and the enemy's striker.
+
+        Vector3 targetDirection = closestCoin.transform.position - transform.position;
+        targetDirection.z = 0f;
+        float targetSpeed = CalculateStrikerSpeed(targetDirection.magnitude);
+
+        // Apply the calculated force to the striker and end the enemy's turn.
+
+        rb.AddForce(targetDirection.normalized * targetSpeed * Time.deltaTime);
+
+        yield return new WaitForSeconds(0.1f);
+        yield return new WaitUntil(() => rb.velocity.magnitude < 0.1f);
+        
+        rb.velocity = Vector2.zero;
+        playerTurn = !playerTurn;
+    }
+
+    float CalculateStrikerSpeed(float distance)
+    {
+        float maxDistance = 2.0f; // Maximum distance the striker can travel
+        float minSpeed = 5000f; // Minimum striker speed
+        float maxSpeed = 10000f; // Maximum striker speed
+
+        float speed = Mathf.Lerp(minSpeed, maxSpeed, distance / maxDistance);
+        return speed;
     }
 
 
@@ -91,6 +139,7 @@ public class StrikerController : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         yield return new WaitUntil(() => rb.velocity.magnitude < 0.1f);
         rb.velocity = Vector2.zero;
+        StrikerSlider.gameObject.SetActive(true);
         if (playerTurn)
         {
             transform.position = new Vector3(StrikerSlider.value, -4.57f, 0);
@@ -98,6 +147,7 @@ public class StrikerController : MonoBehaviour
         else
         {
             transform.position = new Vector3(StrikerSlider.value, 3.45f, 0);
+            StartCoroutine(EnemyTurn());
         }
     }
 }
