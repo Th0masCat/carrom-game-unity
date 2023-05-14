20d37623cbd57d664c33e1db14dc7e95e27492b6
Th0masCat <catsaredogstoo@gmail.com>
2023-05-15 02:05:22 +0530

    finished opponent system

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 
 public class BoardScript : MonoBehaviour
 {
+    public static int scorePlayer1 = 0;
+    public static int scorePlayer2 = 0;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         switch (other.gameObject.tag)
         {
             case "Striker":
+                if (StrikerController.playerTurn == true)
+                {
+                    scorePlayer1--;
+                }
+                else
+                {
+                    scorePlayer2--;
+                }
+
                 Debug.Log("Striker Entered");
                 other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 break;
             case "Black":
+                scorePlayer1++;
+
                 Debug.Log("Black Entered");
                 Destroy(other.gameObject);
                 break;
             case "White":
+                scorePlayer2++;
                 Debug.Log("White Entered");
                 Destroy(other.gameObject);
                 break;
             case "Queen":
+
+                if (StrikerController.playerTurn == true)
+                {
+                    scorePlayer1 += 2;
+                }
+                else
+                {
+                    scorePlayer2 += 2;
+                }
                 Debug.Log("Queen Entered");
                 Destroy(other.gameObject);
                 break;
--- Assets/Scripts/StrikerController.cs
+++ Assets/Scripts/StrikerController.cs
@@ -10,7 +10,7 @@ public class StrikerController : MonoBehaviour
     Slider StrikerSlider;
 
     [SerializeField]
-    float strikerSpeed = 10000f;
+    float strikerSpeed = 1000f;
 
     [SerializeField]
     Transform strikerForceField;
@@ -19,6 +19,8 @@ public class StrikerController : MonoBehaviour
     Vector2 direction;
 
     Rigidbody2D rb;
+
+    public static bool playerTurn = true;
 
     private void Start()
     {
@@ -46,38 +48,56 @@ public class StrikerController : MonoBehaviour
 
         strikerForceField.gameObject.SetActive(false);
         isCharging = false;
-        Vector3 direction =  transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         direction.z = 0f;
         rb.AddForce(direction * strikerSpeed * Time.deltaTime);
+        playerTurn = !playerTurn;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!isCharging)
+        {
+            return;
+        }
+
+        Vector3 direction = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction.z = 0f;
+
+        strikerForceField.LookAt(transform.position + direction);
+
+        float scaleValue = Vector3.Distance(transform.position, transform.position + direction);
+        strikerForceField.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0) && isCharging)
+
+        if (rb.velocity.magnitude > 0.1f)
         {
-            Vector3 direction = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            direction.z = 0f;
-
-            strikerForceField.LookAt(transform.position + direction);
-
-            float scaleValue = Vector3.Distance(transform.position, transform.position + direction);
-            strikerForceField.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
-        }
-
-        if(rb.velocity.magnitude > 0.1f){
             StrikerSlider.gameObject.SetActive(false);
         }
-
-
-        if(!isCharging && rb.velocity.magnitude < 0.1f){
-            StrikerXPos();
+        else if (!isCharging)
+        {
+            StartCoroutine(StrikerXPos());
             StrikerSlider.gameObject.SetActive(true);
         }
 
     }
 
-    public void StrikerXPos()
+
+    public IEnumerator StrikerXPos()
     {
-        transform.position = new Vector3(StrikerSlider.value, -4.57f, 0);
+        yield return new WaitForSeconds(0.1f);
+        yield return new WaitUntil(() => rb.velocity.magnitude < 0.1f);
+        rb.velocity = Vector2.zero;
+        if (playerTurn)
+        {
+            transform.position = new Vector3(StrikerSlider.value, -4.57f, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(StrikerSlider.value, 3.45f, 0);
+        }
     }
 }
